1ecba8deb49bbd5a33775a82712a64d2ed5b1744
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-12-11 19:54:54 +0545

    Inital commit with failed test

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,21 @@
+﻿namespace PrimeServices.Tests
+{
+    [TestClass]
+    public class PrimeService_IsPrimeShould
+    {
+        public readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [TestMethod]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            bool result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+}
---
+++ PrimeServices/PrimeService.cs
@@ -0,0 +1,9 @@
+﻿
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        throw new NotImplementedException("Write tests first!");
+    }
+}
+
